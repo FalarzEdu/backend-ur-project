0d15cfe4b262b530e562cd3319a5109f27b0da28
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2024-11-26 14:58:34 -0300

    add home admin route

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Controllers\AuthController;
+use App\Controllers\UserController;
 use Core\Router\Route;
 
 // Authentication
 Route::get('/login', [AuthController::class, 'new'])->name('users.login');
 Route::post('/login', [AuthController::class, 'new'])->name('users.login');
+
+Route::get('/homeAdmin', [UserController::class, 'index'])->name('users.home');
