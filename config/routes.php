56f14f5cc949b2bfc7e6ff97e5929a4393b36ace
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2024-11-30 13:34:11 -0300

    add logout

--- config/routes.php
+++ config/routes.php
@@ -8,6 +8,7 @@ use Core\Router\Route;
 Route::get('/', [AuthController::class, 'new'])->name('users.login');
 Route::post('/', [AuthController::class, 'authenticate'])->name('users.login');
 
-Route::middleware('auth:user')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/home', [UserController::class, 'new'])->name('users.home');
+    Route::get('/logout', [AuthController::class, 'destroy'])->name('admins.logout');
 });
