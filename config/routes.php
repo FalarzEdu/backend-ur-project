d2686ee6911816d633dde69ab1be093409a3724a
Eduardo Falarz <55895007+FalarzEdu@users.noreply.github.com>
2024-12-01 00:03:01 -0300

    Revert "@pablo/10/feature/admin home"

--- config/routes.php
+++ config/routes.php
@@ -1,15 +1,8 @@
 <?php
 
 use App\Controllers\AuthController;
-use App\Controllers\UserController;
-use App\Controllers\AdminController;
 use Core\Router\Route;
 
 // Authentication
 Route::get('/login', [AuthController::class, 'new'])->name('users.login');
 Route::post('/login', [AuthController::class, 'new'])->name('users.login');
-
-Route::get('/', [UserController::class, 'index'])->name('users.home');
-Route::get('/logout', [AuthController::class, 'destroy'])->name('users.logout');
-
-Route::get('/homeAdmin', [AdminController::class, 'index'])-> name('admins.home');
--- database/Populate/populate.php
+++ database/Populate/populate.php
@@ -4,8 +4,6 @@ require __DIR__ . '/../../config/bootstrap.php';
 
 use Core\Database\Database;
 use Database\Populate\UsersPopulate;
-use Database\Populate\AdminsPopulate;
 
 // Database::migrate();
 UsersPopulate::populate();
-AdminsPopulate::populate();
