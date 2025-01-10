1d778219881574bd025e31221be871c8220d03c2
Eduardo Falarz <eduardofalarz23@gmail.com>
2025-01-10 18:40:05 -0300

    Added random addition of 10 feedbacks to the db populate command.

--- database/Populate/populate.php
+++ database/Populate/populate.php
@@ -5,9 +5,11 @@ require __DIR__ . '/../../config/bootstrap.php';
 use Core\Database\Database;
 use Database\Populate\UsersPopulate;
 use Database\Populate\AdminsPopulate;
+use Database\Populate\FeedbacksPopulate;
 
 Database::drop();
 Database::create();
 Database::migrate();
 UsersPopulate::populate();
 AdminsPopulate::populate();
+FeedbacksPopulate::populate();
