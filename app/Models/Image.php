604bda9e8a4f4632960d08c2bc66de99f9e4e073
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-02-05 02:32:29 -0300

    create model image

---
+++ app/Models/Image.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Core\Database\ActiveRecord\Model;
+use DateTime;
+use Lib\Validations;
+
+/**
+ * @property int $feedback_id;
+ * @property string $path;
+ */
+class Image extends Model
+{
+    protected static string $table = 'feedback_images';
+
+    protected static array $columns = [
+        'feedback_id',
+        'path'
+    ];
+
+    protected ?int $id = null;
+    protected ?int $feedback_id = null;
+    protected ?string $path = null;
+
+    public function __construct(array $params)
+    {
+        $this->feedback_id = $params['feedback_id'];
+        parent::__construct(params: $params);
+    }
+
+    public function validates(): void
+    {
+        Validations::notEmpty(attribute: 'feedback_id', obj: $this);
+        Validations::notEmpty(attribute: 'path', obj: $this);
+    }
+}
